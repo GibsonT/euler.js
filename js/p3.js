1ccd9da7a6512a3a610fbd1b3e27ce2bb6d11a10
Travis Gibson <gibsontk1@gmail.com>
2016-10-24 23:12:06 -0400

    Space out comments better

--- js/p3.js
+++ js/p3.js
@@ -16,9 +16,12 @@ function isPrime(num) {
 
 function checkFactors(factor) {
   const primes = [];
-  
-  // We're using the square root of the number (rounded up) to reduce the number of values to check, which
-  // speeds up the runtime of the function significantly. This is based on the Sieve of Eratosthenes algorithm.
+
+  // We're using the square root of the number (rounded up) to reduce the
+  // number of values to check, which speeds up the runtime of the function
+  // significantly.
+  //
+  // This is based on the Sieve of Eratosthenes algorithm.
   for (let i = 2; i <= Math.ceil(Math.sqrt(factor)); i++) {
     while (factor % i !== 0 && i <= Math.ceil(Math.sqrt(factor))) {
       i++;
@@ -27,22 +30,22 @@ function checkFactors(factor) {
     if (i >= Math.ceil(Math.sqrt(factor))) {
       break;
     }
-    
-    // We're starting from the bottom up, so if factor / i is prime, then, logically, factor / i is the
-    // greatest prime factor.
+
+    // We're starting from the bottom up, so if factor / i is prime,
+    // then, logically, factor / i is the greatest prime factor.
     if (isPrime(factor / i)) {
       return factor / i;
     }
-    
+
     // Otherwise, we push i into the primes array and continue.
     if (isPrime(i)) {
       primes.push(i);
     }
   }
-  
+
   // Sort primes into descending order by value.
   primes.sort((a, b) => (b - a));
-  
+
   // If primes has at least one value, then the first value is the greatest prime factor.
   // Otherwise, the limit given itself is prime!
   return primes.length > 0 ? primes[0] : factor;
